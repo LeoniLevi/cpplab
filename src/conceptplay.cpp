9f84fb5536d41725e0e1a53a0a7080938aac9bbd
LeoniLevi <leonilevi@gmail.com>
2023-07-10 13:28:19 +0300

    concept HasLenFun (using global func)

--- src/conceptplay.cpp
+++ src/conceptplay.cpp
@@ -1,5 +1,13 @@
 #include "conceptplay.h"
 
+
+
+#include <numeric>
+#include <concepts>
+#include <vector>
+
+#include <string>
+
 template<typename T>
 requires std::integral<T> || std::floating_point<T>
 constexpr double Average(const std::vector<T>& vec)
@@ -14,12 +22,33 @@ concept HasLength = requires(T v)
     {v.length()} -> std::convertible_to<double>;
 };
 
+// functions of built-in types is to be declared to be visible by concept 'HasLengthFun'
+// (because of ADL: "argument-dependent lookup" (aka Koenig Lookup))
+double Len(const std::string& s);
+double Len(int n);
+
+template<typename T>
+concept HasLenFun = requires(T v)
+{
+    //{Len(v)} -> std::convertible_to<double>;
+    Len(v);
+};
+
 template<HasLength T>
-double TotalLength( std::vector<T>& vec)
+double TotalLength(const std::vector<T>& vec)
 {
     double len = 0.0;
     for (auto & v : vec)
         len += v.length();
+    return len;
+}
+
+template<HasLenFun T>
+double TotalLenFun(const std::vector<T>& vec)
+{
+    double len = 0.0;
+    for (auto& v : vec)
+        len += Len(v);
     return len;
 }
 
@@ -32,8 +61,26 @@ class Vector2D {
     double y;
 public:
     Vector2D(double x, double y) : x(x), y(y) {}
-    double length() { return sqrt(x * x + y * y); }
+    double length() const { return sqrt(x * x + y * y); }
 };
+double Len(const Vector2D& v) { return v.length(); }
+static_assert(HasLenFun<Vector2D>);
+
+
+struct Point {
+    int x;
+    int y;
+};
+double Len(const Point& p) { return p.x + p.y; }
+static_assert(HasLenFun<Point>);
+
+// Next two funcs(with builtin params) are to be declared Before 'HasLengthFun' definition !!
+double Len(int n) { return n; }
+static_assert(HasLenFun<int>);
+
+double Len(const std::string& s) { return (double)s.length(); }
+static_assert(HasLenFun<std::string>);
+
 
 void play_concepts()
 {
@@ -49,10 +96,23 @@ void play_concepts()
     //double avgs = Average(strings); // ERROR!
     double slen = TotalLength(strings);
 
-    std::vector gvects{ Vector2D(1.1, 1.1), Vector2D{2.2, 2.2}, Vector2D{3.3, 3.3} };
+    //std::vector gvects{ Vector2D(1.1, 1.1), Vector2D{2.2, 2.2}, Vector2D{3.3, 3.3} };
+    std::vector< Vector2D> gvects{ {1.1, 1.1}, { 2.2, 2.2 }, { 3.3, 3.3 } };
     double vlen = TotalLength(gvects);
     printf(" ~~ TotalLen: ofStrings=%f; ofVector2Ds=%f\n", slen, vlen);
 
-    //double dlen = TotalLength(doubles); // ERROR!
+    std::vector<Point> points{ {3, 4}, { 1, 2 }, { 5, 6 }};
+
+    double nlens = TotalLenFun(ints); // ERROR!??
+    printf(" ~~ TotalLenFun result: ofInts=%f\n", nlens);
+    double vlens = TotalLenFun(strings); // ERROR!??
+    printf(" ~~ TotalLenFun result: ofStrings=%f\n", vlens);
+    double vlen2 = TotalLenFun(gvects);
+    printf(" ~~ TotalLenFun result: ofVector2Ds=%f\n", vlen2);
+
+    //double dlen = TotalLengthFun(doubles); // ERROR!
+    //printf(" ~~ TotalLengthFun result: ofDoubles=%f\n", dlen);
+    double ptlens = TotalLenFun(points); // ERROR!??
+    printf(" ~~ TotalLengthFun result: ofPoints=%f\n", ptlens);
 
 }
--- src/conceptplay.h
+++ src/conceptplay.h
@@ -1,8 +1,4 @@
 #pragma once
 
-#include <numeric>
-#include <concepts>
-#include <vector>
-
 
 void play_concepts();
