80dbadd0820247342b31879a903b2d7131002a6e
LeoniLevi <leoni.levi@gmail.com>
2024-10-03 10:14:20 +0300

    experimenting with std::expected

--- main.cpp
+++ main.cpp
@@ -9,8 +9,66 @@
 
 #include <iostream>
 #include <fstream>
+
+
+
 #include <stdio.h>
 #include <string.h>
+
+#if defined(_MSC_VER)
+#   include <print>
+#	include <expected>
+#	include <iomanip>
+
+enum class parse_error
+{
+	invalid_input,
+	overflow
+};
+
+auto parse_number(std::string_view& str) -> std::expected<double, parse_error>
+{
+	const char* begin = str.data();
+	char* end;
+	double retval = std::strtod(begin, &end);
+
+	if (begin == end)
+		return std::unexpected(parse_error::invalid_input);
+	else if (std::isinf(retval))
+		return std::unexpected(parse_error::overflow);
+
+	str.remove_prefix(end - begin);
+	return retval;
+}
+
+void test_expected()
+{
+	puts(" === test_expected - Started...");
+
+	auto process = [](std::string_view str)
+		{
+			std::cout << "str: " << std::quoted(str) << ", ";
+			if (const auto num = parse_number(str); num.has_value())
+				std::cout << "value: " << *num << '\n';
+			// If num did not have a value, dereferencing num
+			// would cause an undefined behavior, and
+			// num.value() would throw std::bad_expected_access.
+			// num.value_or(123) uses specified default value 123.
+			else if (num.error() == parse_error::invalid_input)
+				std::cout << "error: invalid input\n";
+			else if (num.error() == parse_error::overflow)
+				std::cout << "error: overflow\n";
+			else
+				std::cout << "unexpected!\n"; // or invoke std::unreachable();
+		};
+
+	for (auto src : { "42", "42abc", "meow", "inf" })
+		process(src);
+
+	puts(" === test_expected - Completed...");
+}
+
+#endif
 
 //template <typename... TArgs>
 //void my_print(TArgs... args);
@@ -46,9 +104,25 @@ void test_file()
 }
 
 
+
 int main()
 {
 	puts("====== CPP-Example app");
+
+	printf(" ~~ __cplusplus: %uu\n", __cplusplus);
+
+#if defined(_MSC_VER) && !defined(__clang__)
+	printf(" ~~ _MSVC_LANG: %d\n", (int)_MSVC_LANG);
+	printf(" ~~ _MSC_VER: %d\n", (int)_MSC_VER);
+
+	std::println("sum of {} and {} is {}", 5, 4, 5 + 4);
+	test_expected();
+
+#elif defined(__GNUC__)
+	printf(" ~~ __GNUC__: %d\n", (int)__GNUC__);
+	printf(" ~~ __GNUC_MINOR__: %d\n", (int)__GNUC_MINOR__);
+	printf(" ~~ __GNUC__: %d\n", (int)__GNUC__);
+#endif
 
 	puts("~~ RUN <my_print(5, \"mysval\", 'c');> :");
 	my_print(5, "mysval", 'c');
