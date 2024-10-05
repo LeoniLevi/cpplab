cc16479a44514deb5645e8ff0e4df4b4ce69bd28
LeoniLevi <leoni.levi@gmail.com>
2024-10-05 10:12:26 +0300

    untabify

--- cpp20-demo/main.cpp
+++ cpp20-demo/main.cpp
@@ -22,50 +22,50 @@
 
 enum class parse_error
 {
-	invalid_input,
-	overflow
+    invalid_input,
+    overflow
 };
 
 auto parse_number(std::string_view& str) -> std::expected<double, parse_error>
 {
-	const char* begin = str.data();
-	char* end;
-	double retval = std::strtod(begin, &end);
+    const char* begin = str.data();
+    char* end;
+    double retval = std::strtod(begin, &end);
 
-	if (begin == end)
-		return std::unexpected(parse_error::invalid_input);
-	else if (std::isinf(retval))
-		return std::unexpected(parse_error::overflow);
+    if (begin == end)
+        return std::unexpected(parse_error::invalid_input);
+    else if (std::isinf(retval))
+        return std::unexpected(parse_error::overflow);
 
-	str.remove_prefix(end - begin);
-	return retval;
+    str.remove_prefix(end - begin);
+    return retval;
 }
 
 void test_expected()
 {
-	puts(" === test_expected - Started...");
+    puts(" === test_expected - Started...");
 
-	auto process = [](std::string_view str)
-		{
-			std::cout << "str: " << std::quoted(str) << ", ";
-			if (const auto num = parse_number(str); num.has_value())
-				std::cout << "value: " << *num << '\n';
-			// If num did not have a value, dereferencing num
-			// would cause an undefined behavior, and
-			// num.value() would throw std::bad_expected_access.
-			// num.value_or(123) uses specified default value 123.
-			else if (num.error() == parse_error::invalid_input)
-				std::cout << "error: invalid input\n";
-			else if (num.error() == parse_error::overflow)
-				std::cout << "error: overflow\n";
-			else
-				std::cout << "unexpected!\n"; // or invoke std::unreachable();
-		};
+    auto process = [](std::string_view str)
+        {
+            std::cout << "str: " << std::quoted(str) << ", ";
+            if (const auto num = parse_number(str); num.has_value())
+                std::cout << "value: " << *num << '\n';
+            // If num did not have a value, dereferencing num
+            // would cause an undefined behavior, and
+            // num.value() would throw std::bad_expected_access.
+            // num.value_or(123) uses specified default value 123.
+            else if (num.error() == parse_error::invalid_input)
+                std::cout << "error: invalid input\n";
+            else if (num.error() == parse_error::overflow)
+                std::cout << "error: overflow\n";
+            else
+                std::cout << "unexpected!\n"; // or invoke std::unreachable();
+        };
 
-	for (auto src : { "42", "42abc", "meow", "inf" })
-		process(src);
+    for (auto src : { "42", "42abc", "meow", "inf" })
+        process(src);
 
-	puts(" === test_expected - Completed...");
+    puts(" === test_expected - Completed...");
 }
 
 #endif
@@ -75,88 +75,88 @@ void test_expected()
 
 void my_print()
 {
-	std::cout << "\n";
+    std::cout << "\n";
 }
 
 template<typename T, typename... TArgs>
 void my_print(T arg, TArgs... args)
 {
-	std::cout << arg << " ";
-	my_print(args...);
+    std::cout << arg << " ";
+    my_print(args...);
 }
 
 void hello()
 {
-	char buf[32] = { 0 };
-	puts("Hello, World!");
-	puts("What is your name?:");
+    char buf[32] = { 0 };
+    puts("Hello, World!");
+    puts("What is your name?:");
     if (fgets(buf, sizeof buf, stdin) != nullptr)
-		buf[strcspn(buf, "\n")] = '\0';
-	printf("Hello, %s!\n", buf[0] ? buf : "Unknown");
+        buf[strcspn(buf, "\n")] = '\0';
+    printf("Hello, %s!\n", buf[0] ? buf : "Unknown");
 }
 
 void test_file()
 {
-	std::ofstream of("cucu.txt");
-	int n = 5;
-	of << "First Line: n=" << n << "\n";
-	of << "Second Line: n*n=" << n * n;
+    std::ofstream of("cucu.txt");
+    int n = 5;
+    of << "First Line: n=" << n << "\n";
+    of << "Second Line: n*n=" << n * n;
 }
 
 
 
 int main()
 {
-	puts("====== CPP-Example app");
+    puts("====== CPP-Example app");
 
-	printf(" ~~ __cplusplus: %uu\n", __cplusplus);
+    printf(" ~~ __cplusplus: %uu\n", __cplusplus);
 
 #if defined(_MSC_VER) && !defined(__clang__)
-	printf(" ~~ _MSVC_LANG: %d\n", (int)_MSVC_LANG);
-	printf(" ~~ _MSC_VER: %d\n", (int)_MSC_VER);
+    printf(" ~~ _MSVC_LANG: %d\n", (int)_MSVC_LANG);
+    printf(" ~~ _MSC_VER: %d\n", (int)_MSC_VER);
 
-	std::println("sum of {} and {} is {}", 5, 4, 5 + 4);
-	test_expected();
+    std::println("sum of {} and {} is {}", 5, 4, 5 + 4);
+    test_expected();
 
 #elif defined(__GNUC__)
-	printf(" ~~ __GNUC__: %d\n", (int)__GNUC__);
-	printf(" ~~ __GNUC_MINOR__: %d\n", (int)__GNUC_MINOR__);
-	printf(" ~~ __GNUC__: %d\n", (int)__GNUC__);
+    printf(" ~~ __GNUC__: %d\n", (int)__GNUC__);
+    printf(" ~~ __GNUC_MINOR__: %d\n", (int)__GNUC_MINOR__);
+    printf(" ~~ __GNUC__: %d\n", (int)__GNUC__);
 #endif
 
-	puts("~~ RUN <my_print(5, \"mysval\", 'c');> :");
-	my_print(5, "mysval", 'c');
-	puts("~~ testing variadic template 'my_print': done");
+    puts("~~ RUN <my_print(5, \"mysval\", 'c');> :");
+    my_print(5, "mysval", 'c');
+    puts("~~ testing variadic template 'my_print': done");
 
-	bool run_hello = false;
-	if (run_hello) {
-		hello();
-		puts(" ~~ hello: done");
-	}
+    bool run_hello = false;
+    if (run_hello) {
+        hello();
+        puts(" ~~ hello: done");
+    }
 
 
-	//return 0;
-	//
+    //return 0;
+    //
 #ifdef USE_CPP20_COROUTINES
-	play_coroutines();
-	puts(" ~~ play_coroutines: done");
+    play_coroutines();
+    puts(" ~~ play_coroutines: done");
 #endif
 
 #ifdef USE_CPP20_RANGES
-	range_play();
-	puts(" ~~ range_play: done");
+    range_play();
+    puts(" ~~ range_play: done");
 #endif
 
 #ifdef USE_CPP17_FILESYSTEM
-	fsplay();
-	puts(" ~~ fsplay: done");
+    fsplay();
+    puts(" ~~ fsplay: done");
 #endif
 
-	play_concepts();
-	puts(" ~~ playconcepts: done");
+    play_concepts();
+    puts(" ~~ playconcepts: done");
 
-	printf("====== program 'CPP-Example': Completed! To exit - click <Enter>:\n");
+    printf("====== program 'CPP-Example': Completed! To exit - click <Enter>:\n");
     {char dummy[32]; fgets(dummy, sizeof dummy, stdin); }
-	return 0;
+    return 0;
 }
-	
+    
